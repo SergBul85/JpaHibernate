4264c26ef6f2abaf02eea45334d07ad1917528c9
SergBul85 <donetsk55555@gmail.com>
2026-04-26 20:54:32 +0300

    L_24_Nullable

--- src/main/java/org/hstn/entity/Student.java
+++ src/main/java/org/hstn/entity/Student.java
@@ -12,7 +12,7 @@ public class Student {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", unique = true)
     private String name;
 
     @Column(name = "surname")
