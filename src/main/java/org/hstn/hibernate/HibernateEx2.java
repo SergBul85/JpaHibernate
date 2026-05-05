21a888691d13c0575e66af575b49f3fbb62447de
SergBul85 <donetsk55555@gmail.com>
2026-05-05 20:20:43 +0300

    L_67_get_entity

---
+++ src/main/java/org/hstn/hibernate/HibernateEx2.java
@@ -0,0 +1,40 @@
+package org.hstn.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hstn.hibernate.entity.Student;
+
+public class HibernateEx2 {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernare.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+        Transaction transaction = session.getTransaction();
+
+        try {
+            transaction.begin();
+
+            Student student1 = session.get(Student.class, 1);
+            System.out.println(student1);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            factory.close();
+        }
+    }
+}
