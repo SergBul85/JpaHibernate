ceb3ab8ff9bd17872765df126ec38c5df4d33f25
SergBul85 <donetsk55555@gmail.com>
2026-05-05 20:34:15 +0300

    L_68_change_entity

---
+++ src/main/java/org/hstn/hibernate/HibernateEx3.java
@@ -0,0 +1,47 @@
+package org.hstn.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hstn.hibernate.entity.Student;
+
+public class HibernateEx3 {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernare.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+        Transaction transaction = session.getTransaction();
+
+//        SessionFactory factory = new Configuration()
+//                .configure("hibernare.cfg.xml")
+//                .addAnnotatedClass(Student.class)
+//                .buildSessionFactory();
+//
+//        Session session = factory.getCurrentSession();
+//        Transaction transaction = session.getTransaction();
+
+        try {
+            transaction.begin();
+
+            Student student1 = session.get(Student.class, 3);
+            student1.setAvgGrade(9.0);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            factory.close();
+        }
+    }
+}
